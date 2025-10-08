3bd34e70657b1ac3cae1699886980fbb4bd3a1a8
Ali-seoudi1 <alimahmoudsalah153@gmail.com>
2025-10-08 03:02:20 +0300

    1st commit

---
+++ build/mujoco_ros2/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ali/Robotics_project/src/mujoco_ros2
+# Build directory: /home/ali/Robotics_project/build/mujoco_ros2
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
